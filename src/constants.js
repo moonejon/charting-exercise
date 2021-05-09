cc0756431910a377f9c846e6cb86920d545254c5
Jonathan Mooney <moonejon@gmail.com>
2021-05-08 20:00:59 -0500

    setting team codes etc

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const TEAM_IDS = {
+  HOU: 117,
+  OAK: 133,
+  SEA: 136,
+  LAA: 108,
+  TEX: 140,
+};
+
+export const LEAGUE_IDS = {
+    AL: 103,
+    NL: 104
+}
+
+export const DIVISION_IDS = {
+    ALW: 200,
+    ALE: 201,
+    ALC: 202,
+    NLW: 203,
+    NLE: 204,
+    NLC: 205
+}
